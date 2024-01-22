0c78f68cf3f170906fbed6258f7c215c952816e6
mariana-cgsilva <cgsilva.mariana@gmail.com>
2024-01-22 15:38:03 -0300

    Adding model of the product

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask     #importando a Classe Flask da biblioteca flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)   #Criar instância do aplicativo flask
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+
+#Modelagem
+#Produto (id, name, price, description)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, )
+    description = db.Column(db.Text, nullable=True)
+
 
 #Rotas pelas quais os usuário comunicarão com a API (endereço = endpoint)
 #Definir uma rota raiz (página inicial) e a função que será executada ao requisitar
